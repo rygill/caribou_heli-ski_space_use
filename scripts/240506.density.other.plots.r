c2a78ce82d4241e95dcfe4108fd200dc68ee6cf6
Ryan Gill <rygill@gmail.com>
2024-11-24 15:25:58 -0800

    Update 240506.density.other.plots.r
    final

--- scripts/240506.density.other.plots.r
+++ scripts/240506.density.other.plots.r
@@ -4,7 +4,10 @@ library(dplyr)
 library(sf)
 library(terra)
 library(ggplot2)
-library(ggridges)
+#library(ggridges)
+
+rm(list = ls())
+gc()
 
 ## -----------------------------
 # commented out lines have been run to generate the data used later in the script.
@@ -70,6 +73,10 @@ rm(bfile, bou.list, b, bou.dir)
 # get rid of an old column
 tenure.bou.df$pred.log = NULL
 
+# just standardize
+tenure.bou.df$std.pred.smc = tenure.bou.df$pred.gam.smc / max(tenure.bou.df$pred.gam.smc, na.rm = TRUE)
+range(tenure.bou.df$std.pred)
+
 # standardize then log
 tenure.bou.df$smc.log = log(tenure.bou.df$pred.gam.smc)
 tenure.bou.df$smc.std.log = tenure.bou.df$smc.log / max(tenure.bou.df$smc.log, na.rm = TRUE)
@@ -92,6 +99,7 @@ xlabs('Temperatures in Lincoln NE in 2016')
 #heli.rsf = heli.rsf[!(heli.rsf$tenure_id == 0),c('pred.gam.bn', 'tenure_id', 'x', 'y')]
 #heli.rsf = st_as_sf(heli.rsf, coords = c('x','y'), crs = "EPSG:3005")
 #herds = rast('../rasters_for_prediction/herd_id.tif')
+#herd.bound = st_read("./data/smc_herd_boundaries_sg_ID.shp")
 
 #herd.bound = dplyr::rename(herd.bound, 
 #                           'herd_id' = 'id')
@@ -139,6 +147,9 @@ heli.herd.df = do.call(rbind, hel.list)
 
 rm(hel.list, hfile, h, hel.dir)
 
+# just standardize
+heli.herd.df$std.pred.heli = heli.herd.df$pred.gam.heli / max(heli.herd.df$pred.gam.heli, na.rm = TRUE)
+
 # standardize then log
 heli.herd.df$hel.log =  log(heli.herd.df$pred.gam.heli)
 heli.herd.df$hel.std.log = heli.herd.df$hel.log / max(heli.herd.df$hel.log, na.rm = TRUE)
@@ -146,12 +157,12 @@ heli.herd.df$hel.std.log = heli.herd.df$hel.log / max(heli.herd.df$hel.log, na.r
 hist(heli.herd.df$hel.std.log)
 
 # recovered from slack:
-ggplot(heli.herd.df, aes(x = std.log, y = HERD_NAME)) + #, tried adding alpha here with no change
-  geom_density_ridges() + # alpha here produces an error
-  xlab("Heli-ski suitability score") +
-  ylab("Caribou herd") +
-  #scale_fill_viridis_c(name = 'SMC rsf /n score', alpha = 0.2)  +# adding alpha = heli.df$extirpated here produces some weird pattern
-  scale_alpha_manual(values = c(0.2,1)) 
+#ggplot(heli.herd.df, aes(x = std.log, y = HERD_NAME)) + #, tried adding alpha here with no change
+#  geom_density_ridges() + # alpha here produces an error
+#  xlab("Heli-ski suitability score") +
+#  ylab("Caribou herd") +
+#  #scale_fill_viridis_c(name = 'SMC rsf /n score', alpha = 0.2)  +# adding alpha = heli.df$extirpated here produces some weird pattern
+#  scale_alpha_manual(values = c(0.2,1)) 
 
 
 # merge the dataframes by their location
@@ -161,25 +172,34 @@ tenure.bou.df$jfield = paste(tenure.bou.df$x, tenure.bou.df$y,sep = '-')
 mega.dat = merge(heli.herd.df, tenure.bou.df, by = 'jfield')
 
 mega.dat = select(mega.dat,
-                  c('licencee', 'HERD_NAME','x.x', 'y.x', 'smc.std.log', 'hel.std.log'))
+                  c('licencee', 'HERD_NAME','x.x', 'y.x', 'std.pred.smc', 'std.pred.heli')) #original 'smc.std.log', 'hel.std.log'
 
 mega.dat = rename(mega.dat, 'x' = 'x.x', 'y' = 'y.x')
 
 h.rds = unique(mega.dat$HERD_NAME)
 
+# dataframe to annotate each figure
+ann.df = data.frame(
+  label = c('smc: > 0.5 \n heli < 0.5', 'smc: > 0.5 \n heli > 0.5', 'smc: < 0.5 \n heli < 0.5', 'smc: < 0.5 \n heli: > 0.5'),
+  x = c(0.06, 0.9, 0.06, 0.9), #
+  y = c(0.95, 0.95, 0.4, 0.4))
+
+library(shadowtext)
 for(h in unique(h.rds)){
   #h = 'Barkerville'
   print(h)
-  ggplot(mega.dat[mega.dat$HERD_NAME == h,]) +
+  ggplot(mega.dat) + #[mega.dat$HERD_NAME == h,]) +
      theme_bw() +
      ggtitle(h) +
      theme(legend.position = 'none',
            plot.title = element_text(hjust = 1)) +
-     geom_point(aes(x = hel.std.log, y = smc.std.log, alpha = 0.05), size = 0.3, stroke = NA) +
+     geom_point(aes(x = std.pred.heli, y = std.pred.smc, alpha = 0.05), size = 0.3, stroke = NA) + # original: x = hel.std.log, y = smc.std.log
      geom_hline(yintercept = 0.5, linetype = 'dashed', color = 'red') +
      geom_vline(xintercept = 0.5, linetype = 'dashed', color = 'red') +
-     labs(x = "Log standardized heli-ski suitability", y = "Log standardized SMC suitability") 
-  ggsave(file = paste0('./figures/mega_figs/',h,'_mega_dat.jpg'),
+     geom_shadowtext(data=ann.df, aes(x=x, y=y, label=label), 
+              size=3, color = 'white') +
+     labs(x = "Standardized heli-ski suitability", y = "Standardized SMC suitability") 
+  ggsave(file = paste0('./figures/mega_figs/not_logged/',h,'_mega_dat_std.jpg'),
          width = 1500,
          height=1011,
          units = "px",
@@ -187,28 +207,163 @@ for(h in unique(h.rds)){
 
 }
 
-# next lines not used
-# overlap
-#over = rast('./predictions/standardized/product_RATA_HELI_std_231122.tif')
-#over.df = terra::as.data.frame(over)
+# crop to heli-ski tenures, so we're only looking at these values within existing tenures, for interests sake
+mega.sf = st_as_sf(mega.dat, crs = 'EPSG:3005', coords = c('x','y'))
+mega.sf = st_crop(mega.sf, ten.shp)
+head(mega.sf)
+mega.sf = as.data.frame(mega.sf)
+str(mega.sf)
 
-#herds = rast('../rasters_for_prediction/herd_id.tif')
-#tenures = rast('../rasters_for_prediction/tenure_id.tif')
-#h.t = c(herds, tenures)
-
-#over.df = extract(h.t, over.df, bind = TRUE, na.rm = TRUE)
+for(l in unique(mega.sf$licencee)){
+  #l = 'CMH Silver'
+  print(l)
+  ggplot(mega.sf[mega.sf$licencee == l,]) +
+    theme_bw() +
+    ggtitle(h) +
+    theme(legend.position = 'none',
+          plot.title = element_text(hjust = 1)) +
+    geom_point(aes(x = std.pred.heli, y = std.pred.smc, alpha = 0.05), size = 0.3, stroke = NA) + # original: x = hel.std.log, y = smc.std.log
+    geom_hline(yintercept = 0.5, linetype = 'dashed', color = 'red') +
+    geom_vline(xintercept = 0.5, linetype = 'dashed', color = 'red') +
+    geom_shadowtext(data=ann.df, aes(x=x, y=y, label=label), 
+                    size=3, color = 'white') +
+    labs(x = "Standardized heli-ski suitability", y = "Standardized SMC suitability") 
+  ggsave(file = paste0('./figures/mega_figs/not_logged/cropped_to_tenures/',l,'_mega_sf_std.jpg'),
+         width = 1500,
+         height=1011,
+         units = "px",
+         dpi = 300)
+  
+}
 
 # reclassify based on quadrants in above figures greater/less than 0.5
+library(terra)
+library(sf)
+library(dplyr)
+library(ggplot2)
+
 heli = rast('./predictions/standardized/HELI_LATE_WINTER_standardized_231121.tif')
+# keeping distance in:
+heli = rast('./predictions/standardized/HELI_LATE_WINTER_standardized_241108_WITH_DISTANCE_LODGE.tif')
+range(heli)
 heli = ifel(heli < 0.5, 1, 3)
 
 smc = rast('./predictions/standardized/RATA_LATE_WINTER_standardized_231113.tif')
+range(smc)
 smc = ifel(smc < 0.5,1,5)
 cstack = c(smc, heli)
 comb = sum(cstack)
 
-writeRaster(comb, './predictions/standardized/reclassed_heli_smc_240506.tif')
+writeRaster(comb, './predictions/standardized/reclassed_heli_smc_240506_WITH_DISTANCE_LODGE.tif')
 # 2 = lower left
 # 4 = lower right
 # 6 = upper left
 # 8	= upper right
+
+# get proportion of each class for each sub-population
+rm(list = ls())
+comb = rast('./predictions/standardized/reclassed_heli_smc_240506.tif')
+#freq(comb)
+# read smc boundaries
+herd.bound = st_read("./data/smc_herd_boundaries_sg_ID.shp")
+
+herd.bound = herd.bound[,c('id','HERD_NAME')]
+herd.bound$id = as.factor(herd.bound$id)
+
+ten.bound = st_read('./data/heliski_tenures_names.shp')
+plot(ten.bound)
+
+comb.ten = crop(comb, ten.bound)
+comb.ten = mask(comb.ten, ten.bound)
+vals_in_tenures = as.data.frame(freq(comb.ten))
+vals_in_tenures$area_km = vals_in_tenures$count *1225 / 1000000
+vals_in_tenures$value = ifelse(vals_in_tenures$value == 2, 'poor SMC, poor heli-ski',
+                               ifelse(vals_in_tenures$value == 4, 'poor SMC, good heli-ski', 
+                                      ifelse(vals_in_tenures$value == 6, 'good SMC, poor heli-ski', 'good SMC, good heli-ski')))
+
+# loop over herds and get the freq values for each herd
+#freq.df = data.frame('layer' = character(), 'value' = factor(), 'count' = numeric(),'sub_pop' = factor())
+freq.df = data.frame()
+for(h in unique(herd.bound$HERD_NAME)){
+  print(h)
+  #h = 'George_Mountain'
+  h.spat = herd.bound[herd.bound$HERD_NAME == h,]
+  crop.freq = crop(comb, h.spat)
+  h.freq = freq(crop.freq)
+  h.freq$herd = h
+  freq.df = rbind(h.freq, freq.df)
+  }
+freq.df = freq.df[,c('herd', 'value','count')]
+
+# summarise the data
+total.count = freq.df %>% # get the total count for each herd
+  group_by(herd) %>%
+  summarise(total = sum(count)) 
+# join the total back to the original dataframe
+freq.dfa = inner_join(freq.df, total.count, by = 'herd')
+# all values including low for both
+freq.dfa = freq.dfa %>%
+  group_by(herd, value) %>%
+  summarise(percent_area = (count/total)*100)
+
+freq.dfa$percent_area = round(freq.dfa$percent_area, 2)
+freq.dfa$description = ifelse(freq.dfa$value == 2, 'low suitability skiing, low suitability SMC',
+                              ifelse(freq.dfa$value == 4, 'high suitability skiing, low suitability SMC',
+                                     ifelse(freq.dfa$value == 6, 'low suitability skiing, high suitability smc', 'high suitability for both')))
+
+# just values that are non-low for both
+# need new totals
+nonlow.count = freq.df[freq.df$value > 2,] %>% # get the total count for each herd
+  group_by(herd) %>%
+  summarise(total = sum(count)) 
+# join the total back to the original dataframe
+freq.dfb = inner_join(freq.df[freq.df$value > 2,], nonlow.count, by = 'herd')
+# all values including low for both
+freq.dfb = freq.dfb %>%
+  group_by(herd, value) %>%
+  summarise(prop_count = (count/total)*100)
+
+freq.dfb$value = as.factor(freq.dfb$value)
+freq.dfb = na.omit(freq.dfb)
+freq.dfb$desc = ifelse(freq.dfb$value == 4, 'good skiing / poor smc',
+                       ifelse(freq.dfb$value == 6, 'poor skiing / good smc', 'good for both'))
+freq.dfb$desc = factor(freq.dfb$desc, levels = c( 'good skiing / poor smc', 'poor skiing / good smc', 'good for both'))
+
+# get some reporting metrics
+prop.metrics = freq.dfb %>%
+  group_by(desc) %>%
+  summarise(mean.prop = mean(prop_count), 
+            sd.prop = sd(prop_count),
+            max.prop = max(prop_count),
+            min.prop = min(prop_count)) %>%
+  mutate(se.prop = sd.prop / sqrt(65),
+         lower.ci.prop = mean.prop - qt(1 - (0.05 / 2), 65 - 1) * se.prop,
+         upper.ci.prop = mean.prop + qt(1 - (0.05 / 2), 65 - 1) * se.prop)
+
+prop.metrics = select(prop.metrics, c('desc', 'mean.prop', 'lower.ci.prop', 'upper.ci.prop'))
+
+# area in total study area
+df.comb = as.data.frame(comb)
+df.comb = df.comb %>%
+  group_by(sum) %>%
+  summarise(n_values = n())
+df.comb$area = (df.comb$n_values *1225) / 1000000
+sum(df.comb$n_values)
+
+desc.cols = c('good skiing / poor smc' = '#31688e', 'poor skiing / good smc' = '#35b779', 'good for both' = '#fde725')
+ggplot(freq.dfb) +
+  geom_bar(aes(x = value, y = prop_count, fill = desc), stat = 'identity') +
+  scale_x_discrete(breaks = c(4,6,8), labels = c()) + # 4 = good hski, poor smc, 6 = poor hski, good smc, 8 = good both
+  scale_fill_manual(values = desc.cols) + 
+  theme_bw() +
+  theme(legend.position = 'top', legend.title = element_blank()) +
+  labs(x = 'habitat quadrant', y = 'percent of each quadrant by herd') +
+  facet_wrap(~herd)
+ggsave(file = paste0('./figures/proportion_by_quadrant_herd250.jpg'),
+       width = 1500,
+       height=1400,
+       units = "px",
+       dpi = 250)
+
+
+# validation - binning rsf values of known locations
